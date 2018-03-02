673ee062032fcbbb9fd8d094f37c40b9d993d667
Michael Poteat <poteat@users.noreply.github.com>
2018-03-02 09:52:01 -0500

    merged changes QQ -> DM

--- qualityQuantifier/dataMetric.cpp
+++ qualityQuantifier/dataMetric.cpp
@@ -1,13 +1,35 @@
 #include <assert.h>
 #include <string>
 #include <functional>
-#include <iostream>
-#include <iomanip>
+#include <sstream>
 
 #include "axis.h"
-#include "mrc.h"
 
 using namespace cbl;
+
+void applyLineData(mrc map, pdb structure, cbl::real deviation, std::string& data)
+{
+	axis line(structure);
+
+	map.applyDeviationThreshold(deviation);
+	map.setHollowBoundary();
+
+	auto avg = [](std::vector<cbl::real> v)
+	{
+		cbl::real sum = 0;
+		auto add = [&sum](cbl::real x) {sum += x; };
+		std::for_each(v.begin(), v.end(), add);
+		return sum / v.size();
+	};
+
+	line.calcError(map, avg);
+
+	std::ostringstream out;
+
+	line.write(out, deviation);
+
+	data += out.str();
+}
 
 int main(int argc, char* argv[])
 {
@@ -16,69 +38,22 @@ int main(int argc, char* argv[])
 
 	mrc map(argv[1]);
 	pdb structure(argv[2]);
-
-	axis line(structure);
-
-	map.applyDeviationThreshold(2);
-	map.setHollowBoundary();
-
-	auto avg = [](std::vector<cbl::real> v)
-	{
-		cbl::real sum;
-		auto add = [&sum](cbl::real x) {sum += x; };
-		std::for_each(v.begin(), v.end(), add);
-		return sum / v.size();
-	};
-
-	line.calcError(map, avg);
-	// line.removeZeroErrors();
-	line.write("scatter.dat", 2);
-
-	system("pause");
+	std::string data_result;
 
 	map.normalize();
 
-	//take regular intervals of the threshold from 1 - 0.5
-	//continue running smaller thresholds, if voxels are greater than a certain amount, discount them and stop decreasing
-	double threshold;
-	threshold = 1.0;
-
-	for (int i = 20; i > -1; i--)
+	for (float t = 0.2f; t <= 2.0f; t += 0.2f)
 	{
-		map.applyThreshold(threshold);
-		threshold -= 0.05;
-
-		//take slices of helixes to form circles of density and find av distance from center, and then av distance from true axis
-		//use new CBL function for the border mrc points
-
-
-
-
-
-
-
+		applyLineData(map, structure, t, data_result);
 	}
 
-	//At the end, output best threshold into file along with helix and beta sheet comparisons
-	std::string mrc_name = argv[1];
-	std::ofstream outFile;
-	for (int i = 0; i < 4; i++)
-	{
-		mrc_name.pop_back;
-	}
-	mrc_name.append(".txt");
+	std::ofstream out_file("scatter.dat");
 
+	out_file << data_result << std::endl;
 
-	//---------------------------------------------------------------------------------------------------------------------------
-	//Example Use of CBL library for reference later
-	//mrc temp = mrc("foo.mrc");
-	//temp.print();
-	//temp.trim(2);
-	//temp.applyThreshold(.019);
-	//temp.write("bar.mrc");
-	//system("pause");
+	out_file.close();
 
-	outFile.close();
+	system("display_scatter_plot.bat");
 
 	return 0;
 }
